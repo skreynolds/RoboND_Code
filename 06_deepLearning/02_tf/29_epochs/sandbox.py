647566226e879c2b79f476005f2f841d6ab39042
Shane Reynolds <shane.k.reynolds@gmail.com>
2017-11-10 15:02:08 +0930

    Minor changes to the sandbox code

--- 06_deepLearning/02_tf/29_epochs/sandbox.py
+++ 06_deepLearning/02_tf/29_epochs/sandbox.py
@@ -26,7 +26,7 @@ def print_epoch_stats(epoch_i, sess, last_features, last_labels):
         current_cost,
         valid_accuracy))
 
-n_input = 784  # MNIST data input (img shape: 28*28)
+n_input = 784  # MNIST data input (img shape: 28*28) FEATURES
 n_classes = 10  # MNIST total classes (0-9 digits)
 
 # Import MNIST data
