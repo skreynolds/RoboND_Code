52337df70c7afffa98d65d1ca92e2d2a6b420444
skreynolds <shane.k.reynolds@gmail.com>
2018-07-29 10:57:09 +0930

    exercise: refresher of Bayes theorem implementation
    The exercise gets the user to complete a computational exercise using Bayes theorem.

---
+++ 08_mcl/05_bayesFiltering/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+	//Given P(POS), P(DOOR|POS) and P(DOOR|¬POS)
+	double a = 0.0002; //P(POS) = 0.002
+	double b = 0.6;    //P(DOOR|POS) = 0.6
+	double c = 0.05;   //P(DOOR|¬POS) = 0.05
+
+	//TODO: Compute P(¬POS) and P(POS|DOOR)
+	double d = 1 - a;            	//P(¬POS)
+	double e = (b*a)/(b*a + c*d);   //P(POS|DOOR)
+
+	//Print Result
+	std::cout << "P(POS|DOOR)= " << e << std::endl;
+
+	return 0;
+}
